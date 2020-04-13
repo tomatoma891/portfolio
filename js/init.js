fd0af1e6b6b3fc40fe174a5db4b280f774d6b3a4
Tamara Sidorova <tomatoma891@gmail.com>
2020-04-12 22:50:21 -0600

    need to fixed header

---
+++ js/init.js
@@ -0,0 +1,13 @@
+// end of document ready
+$(document).ready(function () {
+  $('.parallax').parallax();
+});
+
+// end of jQuery name space
+$(document).ready(function () {
+  $('.carousel').carousel();
+});
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true
+});
